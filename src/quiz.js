b5dbc1c6035701090c61a66f8316d1438eaafcdd
Barbara Szott <barbara.aleksy@gmail.com>
2019-08-01 09:58:01 +0200

    Create simple data input

---
+++ src/quiz.js
@@ -0,0 +1,16 @@
+const quiz = [
+	{
+		question : 'First four numbers of Fibbonacci sequence are..?',
+		answer   : '1123'
+	},
+	{
+		question : "How long is girraffe's tongue? [cm]",
+		answer   : '0045'
+	},
+	{
+		question : 'In what year did Titanic sink?',
+		answer   : '1912'
+	}
+];
+
+export default quiz;
