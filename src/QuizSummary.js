efc3c2c3906685d921ad96a20e8afe22e6f2023a
Barbara Szott <barbara.aleksy@gmail.com>
2019-08-12 15:45:31 +0200

    Create list with questions from quiz in summary

--- src/QuizSummary.js
+++ src/QuizSummary.js
@@ -6,28 +6,30 @@ class QuizSummary extends Component {
 		const { correctAnswers, questionsCount, quizSet } = this.props;
 		return (
 			<div>
-				<h1>Congrats!</h1>
-				<p>
+				<h1 className="text-with-border">Congrats!</h1>
+				<p className="score">
 					Score: {correctAnswers.length} / {questionsCount}
 				</p>
 				<div>
-					<h2>Questions in this quiz were:</h2>
-					<ol className="summary-answers">
+					<h2 className="text-with-border">Questions in this quiz were:</h2>
+					<ul className="fa-ul summary-answers">
 						{quizSet.map((quiz, i) => (
-							<li
-								key={i}
-								className={
-									correctAnswers.length && correctAnswers.includes(i) ? (
-										'summary-answers_correct'
-									) : (
-										'summary-answers_incorrect'
-									)
-								}
-							>
+							<li key={i}>
+								<span className="fa-li">
+									<i
+										className={
+											correctAnswers.length && correctAnswers.includes(i) ? (
+												'fas fa-check-circle check-icon correct'
+											) : (
+												'fas fa-times-circle check-icon incorrect'
+											)
+										}
+									/>
+								</span>
 								{quiz.question}
 							</li>
 						))}
-					</ol>
+					</ul>
 				</div>
 			</div>
 		);
