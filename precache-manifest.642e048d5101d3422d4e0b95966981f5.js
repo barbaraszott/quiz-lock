dd5c3ae46283b8f031dcf5e96fca25bee68719f7
Barbara Szott <barbara.aleksy@gmail.com>
2019-08-27 12:52:43 +0200

    Updates

---
+++ precache-manifest.642e048d5101d3422d4e0b95966981f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d91462f791b20783e254c8550df27436",
+    "url": "/quiz-lock/index.html"
+  },
+  {
+    "revision": "bf99503db92d4bba1fab",
+    "url": "/quiz-lock/static/css/main.6b3a39d6.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/quiz-lock/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "bf99503db92d4bba1fab",
+    "url": "/quiz-lock/static/js/main.ad10653d.chunk.js"
+  },
+  {
+    "revision": "b4b9579a3f3d515ad4eb",
+    "url": "/quiz-lock/static/js/runtime~main.80def91c.js"
+  },
+  {
+    "revision": "11eb8f32ab8b8bdbe5bc564240b74215",
+    "url": "/quiz-lock/static/media/wood-background.11eb8f32.png"
+  }
+]);
